d6d1858d3c7cc89828c6ffddb35f371057b66dcb
Chenggang Zhao <lyricz@yeah.net>
2021-09-11 17:56:45 +0800

    Add weight initialization and evaluation

--- examples/train.cpp
+++ examples/train.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
     desc.add_options()
         ("path", po::value<std::string>()->required(), "CIFAR-10 path")
         ("lr", po::value<double>()->default_value(0.1), "Learning rate")
-        ("epochs", po::value<int>()->default_value(200), "Number of epochs")
+        ("epochs", po::value<int>()->default_value(5), "Number of epochs")
         ("batch_size", po::value<int>()->default_value(8), "Batch size")
         ("workers", po::value<int>()->default_value(4), "Number of workers to process data");
     po::variables_map args;
@@ -20,15 +20,24 @@ int main(int argc, char** argv) {
 
     // Read dataset
     std::cout << "Reading dataset ... " << std::flush;
-    auto dataset = rnlt::CIFAR10(args["path"].as<std::string>())
+    auto train_dataset = rnlt::CIFAR10(args["path"].as<std::string>())
         .map(torch::data::transforms::Normalize<>({0.4914, 0.4822, 0.4465},
                                                   {0.2023, 0.1994, 0.2010}))
         .map(torch::data::transforms::Stack<>());
-    auto data_loader = torch::data::make_data_loader(
-            std::move(dataset), torch::data::DataLoaderOptions()
+    auto train_data_loader = torch::data::make_data_loader(
+            std::move(train_dataset), torch::data::DataLoaderOptions()
             .batch_size(args["batch_size"].as<int>())
             .workers(args["workers"].as<int>())
             .enforce_ordering(true));
+    auto test_dataset = rnlt::CIFAR10(args["path"].as<std::string>(), false)
+            .map(torch::data::transforms::Normalize<>({0.4914, 0.4822, 0.4465},
+                                                      {0.2023, 0.1994, 0.2010}))
+            .map(torch::data::transforms::Stack<>());
+    auto test_data_loader = torch::data::make_data_loader(
+            std::move(test_dataset), torch::data::DataLoaderOptions()
+                    .batch_size(args["batch_size"].as<int>())
+                    .workers(args["workers"].as<int>())
+                    .enforce_ordering(true));
     std::cout << "OK!" << std::endl;
 
     // Load model and device
@@ -47,17 +56,36 @@ int main(int argc, char** argv) {
                                     .momentum(0.9)
                                     .weight_decay(1e-4));
     torch::nn::CrossEntropyLoss criterion;
-    for (int i = 1, n = args["epochs"].as<int>(); i <= n; ++ i) {
-        for (auto& batch: *data_loader) {
+    for (int i = 1, steps = 1, n = args["epochs"].as<int>(); i <= n; ++ i) {
+        // Train
+        model.train();
+        for (auto& batch: *train_data_loader) {
             auto images = batch.data.to(device);
             auto labels = batch.target.to(device);
             optimizer.zero_grad();
-            auto out = model.forward(images);
-            auto loss = criterion(out, labels);
-            std::cout << " > Loss: " << loss.item<float>() << std::endl;
+            auto outs = model.forward(images);
+            auto loss = criterion(outs, labels);
+            std::cout << "\r > Loss@[" << steps ++ << ", " << i << "/" << n << "]: " << loss.item<float>() << std::flush;
             loss.backward();
             optimizer.step();
         }
+        std::cout << std::endl;
+
+        // Evaluate
+        model.eval();
+        torch::NoGradGuard no_grad;
+        int64_t num_samples = 0, num_corrects = 0;
+        for (auto& batch: *test_data_loader) {
+            auto images = batch.data.to(device);
+            auto labels = batch.target.to(device);
+            auto outs = model.forward(images);
+            auto predictions = std::get<1>(torch::max(outs, 1));
+            num_corrects += torch::sum(predictions == labels).item<int64_t>();
+            num_samples += images.size(0);
+            std::cout << "\r > Accuracy@" << i << ": " << (double) num_corrects / (double) num_samples
+                      << " (" << num_corrects << "/" << num_samples << ")" << std::flush;
+        }
+        std::cout << std::endl;
     }
     return 0;
 }
